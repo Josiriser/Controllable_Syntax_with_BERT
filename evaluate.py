eceac54b9011b3a12840664d0f225514cbc627f1
Yao <josiriser@gmail.com>
2021-02-04 01:38:04 +0000

    取token

--- evaluate.py
+++ evaluate.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 import sacrebleu
+from transformers import BertTokenizer
 
 def main():
 
@@ -11,9 +12,9 @@ def main():
     args = parser.parse_args()
 
     if args.input_file_path==None:
-        args.input_file_path="mingda_chen_dataset/dev_input.txt"
+        args.input_file_path="mingda_chen_dataset/test_input.txt"
     if args.ref_file_path==None:
-        args.ref_file_path="mingda_chen_dataset/dev_ref.txt"
+        args.ref_file_path="mingda_chen_dataset/test_ref.txt"
 
     ref,sys=read_data(args.input_file_path,args.ref_file_path)
     eva_bleu(ref,sys)
@@ -26,13 +27,15 @@ def eva_bleu(ref,sys):
     
 
 def read_data(input_file_path,ref_file_path):
+    tokenizer = BertTokenizer(vocab_file='bert-base-uncased-vocab.txt')
     ref1=[]
     ref2=[]
     ref=[]
     with open(input_file_path,"r",encoding='utf-8') as f:
         for line in f:
-            ref1.append(line.split("\t")[0])
-            ref2.append(line.split("\t")[1].strip("\n"))
+
+            ref1.append(token_process(line.split("\t")[0],tokenizer))
+            ref2.append(token_process(line.split("\t")[1].strip("\n"),tokenizer))
         ref.append(ref1)
         ref.append(ref2)
     sys=[]
@@ -41,6 +44,13 @@ def read_data(input_file_path,ref_file_path):
             sys.append(line.strip("\n"))
     return ref,sys
 
+def token_process(sentence,tokenizer):
+    token_list = tokenizer.tokenize(sentence)
+    text=""
+    for token in token_list:
+        text=text+token+" "
+
+    return text
 
 
 if __name__ == "__main__":
